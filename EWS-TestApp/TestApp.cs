c0dc57953d3fbed7cbee7857f5fa8eadce44a508
Emil Kalchev <emil.kalchev@mobisystems.com>
2022-08-24 10:47:18 +0300

    OAuth Examples

--- EWS-TestApp/TestApp.cs
+++ EWS-TestApp/TestApp.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Exchange.WebServices.Data;
+using Microsoft.Identity.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,18 +10,60 @@ using Task = System.Threading.Tasks.Task;
 
 namespace EWS_TestApp
 {
+    // Autodiscover overview: https://docs.microsoft.com/en-us/exchange/client-developer/exchange-web-services/autodiscover-for-exchange
+
+    // OAUTH:
+    // OAuth authentication examples: https://docs.microsoft.com/en-us/exchange/client-developer/exchange-web-services/how-to-authenticate-an-ews-application-by-using-oauth
+    // Configure app for OAuth: https://www.michev.info/Blog/Post/3180/exchange-api-permissions-missing
+    // To be able to use OAuth with user and password, set allowPublicClient to true in the app manifest
+
     internal class TestApp
     {
-        private const string username = "emil.kalchev@mobiexch.net";
-        private const string password = "Password1";
+        class Settings
+        {
+            public string ExchangeUrl { get; private set; }
+            public string AutoDiscoverUrl { get; private set; }
+            public string UserName { get; private set; }
+            public string Password { get; private set; }
+
+            private Settings()
+            {
+            }
+
+            public static Settings Outlook =>
+                new Settings()
+                {
+                    ExchangeUrl = "https://outlook.office365.com/EWS/Exchange.asmx",
+                    AutoDiscoverUrl = "https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc",
+                    UserName = "neven.mollov@mobisystems.com",
+                    Password = "Password1",
+                };
+
+            public static Settings MobiTest =>
+                new Settings()
+                {
+                    ExchangeUrl = "https://vm-exchange01.mobiexch.net/EWS/Exchange.asmx",
+                    AutoDiscoverUrl = "https://vm-exchange01.mobiexch.net/autodiscover/autodiscover.svc",
+                    UserName = "neven.mollov@mobiexch.net",
+                    Password = "Password1",
+                };
+        }
+
+        private static readonly Settings settings = Settings.Outlook;
+        private static readonly string clientId = "fffb71af-18e3-4e7b-886d-63eb471f8ee5";
+        private static readonly string clientSecret = "UdT8Q~sNgZJ810373wZ7w~uANtKRwTiFDKU5laTL";
+        private static readonly string tenantId = "fe426553-582a-4aae-916d-11edb985b6c1";
 
         public async Task RunAsync()
         {
             ServicePointManager.ServerCertificateValidationCallback = CertificateValidationCallBack;
 
-            //ExchangeService service = CreateServiceUsingAutoDiscovery();
-            ExchangeService service = CreateServiceForUrl("https://vm-exchange01.mobiexch.net/EWS/Exchange.asmx");
-            var properties = new PropertySet(BasePropertySet.IdOnly, 
+            ExchangeService service = CreateServiceUsingAutoDiscovery();
+            //ExchangeService service = CreateServiceForUrl();
+            //ExchangeService service = await CreateServiceUsingOAuthToken();
+            //ExchangeService service = await CreateServiceUsingOAuthToken2();
+
+            var properties = new PropertySet(BasePropertySet.IdOnly,
                                                 FolderSchema.DisplayName,
                                                 new ExtendedPropertyDefinition(0x10f4, MapiPropertyType.Boolean));
             service.BeginSubscribeToStreamingNotificationsOnAllFolders(a =>
@@ -54,7 +97,7 @@ namespace EWS_TestApp
                 }
             }, null, new FolderId(WellKnownFolderName.Root), properties, null);
 
-        
+
             await Task.Delay(100000);
         }
 
@@ -119,26 +162,119 @@ namespace EWS_TestApp
             }
         }
 
-        private ExchangeService CreateServiceForUrl(string url)
+        private ExchangeService CreateServiceForUrl()
         {
             // Create the binding.
             ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP1);
             // Set the credentials for the on-premises server.
-            service.Credentials = new NetworkCredential(username, password);
+            service.Credentials = new NetworkCredential(settings.UserName, settings.Password);
             // Set the URL.
-            service.Url = new Uri(url);
+            service.Url = new Uri(settings.ExchangeUrl);
             return service;
         }
 
         private ExchangeService CreateServiceUsingAutoDiscovery()
         {
-            // Create the binding.
-            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP1);
-            // Set the credentials for the on-premises server.
-            service.Credentials = new NetworkCredential(username, password);
-            // Set the URL.
-            service.AutodiscoverUrl(username);
-            return service;
+            try
+            {
+                // Create the binding.
+                ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP1);
+                // Set the credentials for the on-premises server.
+                service.Credentials = new NetworkCredential(settings.UserName, settings.Password);
+                // Set the URL.
+                service.AutodiscoverUrl(settings.UserName);
+                return service;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// App-only authentication
+        /// </summary>
+        /// <returns></returns>
+        private async Task<ExchangeService> CreateServiceUsingOAuthToken()
+        {
+            try
+            {
+                // Using Microsoft.Identity.Client 4.22.0
+                var cca = ConfidentialClientApplicationBuilder
+                    .Create(clientId)
+                    .WithClientSecret(clientSecret)
+                    .WithTenantId(tenantId)
+                    .Build();
+
+                // The permission scope required for EWS access
+                var ewsScopes = new string[] { "https://outlook.office365.com/.default" };
+
+                //Make the token request
+                var authResult = await cca.AcquireTokenForClient(ewsScopes).ExecuteAsync();
+
+
+
+                // Configure the ExchangeService with the access token
+                ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP1);
+                service.Url = new Uri(settings.ExchangeUrl);
+                service.Credentials = new OAuthCredentials(authResult.AccessToken);
+
+                service.ImpersonatedUserId = new ImpersonatedUserId(ConnectingIdType.SmtpAddress, settings.UserName);
+                //Include x-anchormailbox header
+                service.HttpHeaders.Add("X-AnchorMailbox", settings.UserName);
+
+                var folders = service.FindFolders(WellKnownFolderName.MsgFolderRoot, new FolderView(10));
+
+                return service;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Delegated authentication
+        /// </summary>
+        /// <returns></returns>
+        private async Task<ExchangeService> CreateServiceUsingOAuthToken2()
+        {
+            try
+            {
+                // Using Microsoft.Identity.Client 4.22.0
+
+                // Configure the MSAL client to get tokens
+                var pcaOptions = new PublicClientApplicationOptions
+                {
+                    ClientId = clientId,
+                    TenantId = tenantId,
+                    RedirectUri = "https://login.microsoftonline.com/common/oauth2/nativeclient",
+                };
+
+                var pca = PublicClientApplicationBuilder
+                    .CreateWithApplicationOptions(pcaOptions).Build();
+
+                // The permission scope required for EWS access
+                var ewsScopes = new string[] { "https://outlook.office365.com/EWS.AccessAsUser.All" };
+
+                // Make the interactive token request
+                var authResult = await pca.AcquireTokenInteractive(ewsScopes).ExecuteAsync();
+
+
+
+                // Configure the ExchangeService with the access token
+                var service = new ExchangeService(ExchangeVersion.Exchange2010_SP1);
+                service.Url = new Uri(settings.ExchangeUrl);
+                service.Credentials = new OAuthCredentials(authResult.AccessToken);
+
+                var folders = service.FindFolders(WellKnownFolderName.MsgFolderRoot, new FolderView(10));
+
+                return service;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
